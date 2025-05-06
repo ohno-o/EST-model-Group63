d89e988ae09c15b3f81079247d4033baf3343dea
Bram de Rooij <b.d.rooij@student.tue.nl>
2025-05-06 16:31:19 +0200

    Added parameters that are used in the Extraction block

--- preprocessing.m
+++ preprocessing.m
@@ -38,6 +38,15 @@ bStorage        = 1e-6/unit("s");  % Storage dissipation coefficient
 
 % extraction system
 aExtraction = 0.1; % Dissipation coefficient
+TurbineEfficiency = 0.40;
+    %water tank parameters
+    wallThickness = 0.2; %meters
+    r_in = 1.5;
+    height = 4;
+    conductivity = 1;   %W/(m*K), realistic value for concrete.
+T_env = 10; %degrees C, average temp in the netherlands
+T_water = 90;   %this should be changed to a function in order to reflect the cooling due to heat loss
+
 
 % transport to demand
 aDemandTransport = 0.01; % Dissipation coefficient
