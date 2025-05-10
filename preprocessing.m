08b3685094624473106cb71a983e4fdba5aa0ae4
Emil Priftaj <e.priftaj@student.tue.nl>
2025-05-10 19:20:57 +0200

    Added storage parameters. The two added matlab blocks inside the storage block calculate the dissipation coefficient of the strorage considering both conduction and radiation.

--- preprocessing.m
+++ preprocessing.m
@@ -31,10 +31,9 @@ aSupplyTransport = 0.01; % Dissipation coefficient
 aInjection = 0.1; % Dissipation coefficient
 
 % storage system
-EStorageMax     = 10.*unit("kWh"); % Maximum energy
+EStorageMax     = 4320000.0*unit("kWh"); % Maximum energy
 EStorageMin     = 0.0*unit("kWh"); % Minimum energy
 EStorageInitial = 4320000.0*unit("kWh"); % Initial energy
-bStorage        = 1e-6/unit("s");  % Storage dissipation coefficient
 
 % extraction system
 aExtraction = 0.1; % Dissipation coefficient
@@ -50,3 +49,26 @@ T_water = 90;   %this should be changed to a function in order to reflect the co
 
 % transport to demand
 aDemandTransport = 0.01; % Dissipation coefficient
+
+
+% Storage parameters
+no_tanks = 2;
+tank_volume = 10000;
+radius_in = 11.7;
+height_in = 23.4;
+salt_temp = 838;  % in kelvin
+env_temp = 293;  % in kelvin
+
+    % Insulation made of 3 layers
+    k1 = 1.2;
+    k2 = 0.14;
+    k3 = 0.12;
+
+    t1 = 0.1;
+    t2 = 0.15;
+    t3 = 0.05;
+    t4 = 0.015;
+
+conv_coeff = 15;
+steel_emissivity = 0.35;
+SB_constant = 5.67 * 10^-8;
